c42bd4c870afb09ec7cc6e67316c359b9220935f
Yaryna Korduba <yaryna.korduba@stud.uni-bamberg.de>
2018-03-12 16:38:12 +0100

    fixed BEM

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { format } from "d3-format";
 
 
 var diameter = 800; //max height
-var width =800;
+var width = 800;
 
 var f = d3.format("d");
 
@@ -19,19 +19,23 @@ var textscale = d3.scaleSqrt()
 
 var scaleRadius = d3.scaleSqrt().domain([6, 50]).range([6, 30]);
 
+ var container = d3.select("body")
+    .append("div")
+    .attr("class", "container");
  
- var header = d3.select("body")
+ var header = container
     .append("header")
-    .append("h3")
+    .attr("class", "container__header")
+    .append("h3") //
     .text("Countries birth rate visualization per 1000 population");
 
- var svg = d3.select("body")
-    .append("svg")
+ var svg = container.append("svg")
     .attr("width", width)
     .attr("height", diameter)
+    .attr("class", "container__svg")
     .append("g")
     .attr("transform", "translate(0,0)")    
-    .attr("class", "bubble");
+    .attr("class", "container__bubble"); //
 
 var simulation = forceSimulation()
     .force("x", d3.forceX(width / 2 + 100).strength(0.05))
@@ -44,15 +48,15 @@ var simulation = forceSimulation()
 
 d3.csv("/data/data.csv", function(data) {
 
-    let tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
+    let tooltip = container.append("div")
+            .attr("class", "container__tooltip") //
             .style("display", "none");
 
 
     var circles = svg.selectAll("country")
         .data(data)
         .enter().append("circle")
-        .attr("class", "country")
+        .attr("class", "container__country") //
         .attr("r", function(d) {
             return scaleRadius(d.birth)})
         .on("click mouseenter mouseover focus", function(d) {
@@ -66,20 +70,20 @@ d3.csv("/data/data.csv", function(data) {
         tooltip.html("country " + d.country + "\n" + d.birth)
             .style("left", (d3.event.pageX) + "px")
             .style("top", d3.event.pageY + "px");
-        })
+            })
         .on("mouseout", function(d) {
             tooltip.transition().style("display", "none");
             d3.select(this).style("stroke-opacity", "0");
-        })
+            })
         .style("fill", function(d) { 
         return color(d.birth); 
-      });
+            });
 
     var text =svg.selectAll("country")
         .data(data)
         .enter()
         .append("text")
-        .attr("class", "countryname")
+        .attr("class", "container__countryname") //
         .text(function(d) {
             return d.code;
         })
@@ -93,7 +97,7 @@ d3.csv("/data/data.csv", function(data) {
                 .duration(2)  
                 .style("opacity", .9)
                 .style("display", "block");  
-            tooltip.html("country " + d.country + "\n" + d.birth)
+            tooltip.html("country " + d.country + "\n" + d.birth) 
             .style("left", (d3.event.pageX) + "px")
             .style("top", d3.event.pageY + "px");
         })
@@ -128,30 +132,36 @@ d3.csv("/data/data.csv", function(data) {
     function sortingOrder(data) {
         return data.sort(function (a, b) {
             return +a.birth - b.birth;
-    });
+        });
     };
 
-    var maxmin = d3.select("body")
-        .data(data)
+    var maxmin = container.data(data)
         .append("div")
-        .attr("class", "maxmin");
+        .attr("class", "info"); //
 
-    var maxminhead = maxmin.append("h3").text("Bound birth rate values:")
+    var maxminhead = maxmin.
+        append("h3").
+        text("Bound birth rate values:")
+        .attr("class", "info__caption");
 
-    var max = maxmin.append("div").attr("class", "maxminval")
+    var max = maxmin.append("div").attr("class", "info__value") //
         .text(function(d) {
             return "maximal value: " + sortingOrder(data)[data.length-1].country +
             " " + sortingOrder(data)[data.length-1].birth + '\n';
             });
-    var min = maxmin.append("div").attr("class", "maxminval")
+
+    var min = maxmin.append("div").attr("class", "info__value") //
         .text(function(d) {
             return "minimal value: " + sortingOrder(data)[0].country + " " +
             sortingOrder(data)[0].birth;
             });
 
-    var datainfo = maxmin.append("div").attr("class", "info")
+    var datainfo = maxmin.append("div")
+        .attr("class", "info__ref") //
         .text( "Data for visualization was taken from ");
+        
         datainfo.append("a")
+            .attr("class", "info__link")
             .attr("href", "http://www.color-hex.com/color/0d47c8")
             .text("World Health Organization");
         
