ff5c4bb140147cd56ccf9310c5e538b076b1a4eb
Yaryna Korduba <yaryna.korduba@cordian.com>
2021-10-28 23:04:41 +0200

    Add fixes to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: path.resolve(__dirname + '/src/index.js'),
     output: {
 		path: path.resolve(__dirname + "/dist"),
 		filename: "index.js"
