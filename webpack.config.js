ab8230db9cc10acb0b5ba175a1a04d46e2412790
Yaryna Korduba <yaryna.korduba@cordian.com>
2021-10-28 22:50:36 +0200

    Add fixes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require("path");
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        
 		path: __dirname + "/dist",
 		filename: "index.js"
-	
     }
 };
